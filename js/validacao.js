ccd060fe33bddec058aa254b12589488f2fec191
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2023-06-01 22:29:28 -0300

    Update

--- js/validacao.js
+++ js/validacao.js
@@ -17,6 +17,8 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         document.body.innerHTML = `
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="J=jogar-novamente" class"btn-jogar">Jogar Novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -37,3 +39,9 @@ function chuteForInvalido(numero) {
 function numeroForMaiorOuMenorQueOValorPermitido(numero){
     return numero > maiorValor || numero < menorValor
 }
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
